2cc8cbe7bc3eb25d13647b18da094e72d6c4c973
James Gill <jamesagill@gmail.com>
2013-02-12 01:24:36 -0800

    Also adding a test for three events.

--- client/tests/eventTests.js
+++ client/tests/eventTests.js
@@ -22,6 +22,21 @@ Meteor.startup(function () {
       record('2.second');
     }
   });
+  Template.hello.events({
+    'click #testButton3' : function () {
+      record('3.first');
+    }
+  });
+  Template.hello.events({
+    'click #testButton3' : function () {
+      record('3.second');
+    }
+  });
+  Template.hello.events({
+    'click #testButton3' : function () {
+      record('3.third');
+    }
+  });
 });
 
 describe("Template.hello.events", function() {
@@ -38,6 +53,13 @@ describe("Template.hello.events", function() {
     document.getElementById("testButton2").click();
     assert.isTrue(recorded['2.second'], 'Should fire second handler.');
     assert.isTrue(recorded['2.first'], 'Should fire first handler.');
+  });
+
+  it("should allow three events to be set", function () {
+    document.getElementById("testButton3").click();
+    assert.isTrue(recorded['3.third'], 'Should fire third handler.');
+    assert.isTrue(recorded['3.second'], 'Should fire second handler.');
+    assert.isTrue(recorded['3.first'], 'Should fire first handler.');
   });
 
 
